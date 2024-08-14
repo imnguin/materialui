30bb58d016c371cdfdc1ba861dc4a96c99737d48
Lâm Xuân Nguyên <100450698+imnguin@users.noreply.github.com>
2024-08-14 18:59:21 +0700

    commit

--- src/views/DashBoard.js
+++ src/views/DashBoard.js
@@ -1,20 +1,21 @@
 import React from "react";
 import '../App.css'
+import { Flex } from "antd";
 const DashBoard = () => {
     return (
         <div className="App">
             <div className="Sider">
-                <div style={{
-                    color : 'white',
-                    height : '7%',
-                    display : 'flex',
-                    alignItems : 'center',
-                    justifyContent : 'center',
-                    fontWeight : 'bold'
-                }}>
-                    Material DashBoard
+                <div className="Sider-header">
+                    Bizlinks Company
                 </div>
                 <hr />
+            </div>
+
+            <div style={{
+                marginLeft : 300,
+                minHeight : '100vh',
+                backgroundColor : 'red',
+            }}>
             </div>
         </div>
     );
